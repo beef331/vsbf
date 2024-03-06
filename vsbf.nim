ef89fc51ebe8dead903383b0c0aa495116d8a00a
Jason Beetham <beefers331@gmail.com>
2024-03-05 18:09:00 -0700

    used nph

--- vsbf.nim
+++ vsbf.nim
@@ -17,31 +17,43 @@ type
 proc `==`(a, b: MyObject): bool {.noSideEffect.} =
   system.`==`(a, b)
 
-let obj = MyObject(
-  x: 0, y: 0, age: 42,
-  name: "Jimbo",
-  talents: ["Jumping", "Running", "", ""],
-  pets: @["Sam", "Diesel"],
-  letters: {'a'..'z'},
-  children: newSeq[MyObject](5))
+let
+  obj =
+    MyObject(
+      x: 0,
+      y: 0,
+      age: 42,
+      name: "Jimbo",
+      talents: ["Jumping", "Running", "", ""],
+      pets: @["Sam", "Diesel"],
+      letters: {'a'..'z'},
+      children: newSeq[MyObject](5),
+    )
 
-var encoder = Encoder.init(true)
-encoder.serialiseRoot(obj)
-encoder.save "/tmp/test.vsbf"
-encoder = Encoder.init(false)
-encoder.serialiseRoot(obj)
-encoder.save "/tmp/test1.vsbf"
+proc main() =
+  var encoder = Encoder.init(true)
+  encoder.serialiseRoot(obj)
+  encoder.save "/tmp/test.vsbf"
+  encoder = Encoder.init(false)
+  encoder.serialiseRoot(obj)
+  encoder.save "/tmp/test1.vsbf"
 
-var
-  fileData = readFile("/tmp/test.vsbf")
-  theFile = @(fileData.toOpenArrayByte(0, fileData.high))
+  var
+    fileData = readFile("/tmp/test.vsbf")
+    theFile = @(fileData.toOpenArrayByte(0, fileData.high))
+  let dataAddr = theFile[0].addr
 
-var decoder = Decoder.init(true, theFile)
-assert decoder.deserialiseRoot(MyObject) == obj
+  var decoder = Decoder.init(true, theFile)
 
-fileData = readFile("/tmp/test1.vsbf")
-var buffer: array[1024, byte]
+  assert decoder.deserialiseRoot(MyObject) == obj
+  var buff = decoder.close()
+  assert buff[0].addr == dataAddr # We are reusing the buffer!
 
-buffer[0..fileData.high] = (fileData.toOpenArrayByte(0, fileData.high))
-var oaDecoder = Decoder.init(false, buffer)
-assert oaDecoder.deserialiseRoot(MyObject) == obj
+  fileData = readFile("/tmp/test1.vsbf")
+  var buffer: array[1024, byte]
+
+  buffer[0..fileData.high] = (fileData.toOpenArrayByte(0, fileData.high))
+  var oaDecoder = Decoder.init(false, buffer)
+  assert oaDecoder.deserialiseRoot(MyObject) == obj
+
+main()
